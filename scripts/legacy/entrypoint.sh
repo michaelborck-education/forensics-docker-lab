d08da0f6cca2072ac90abda4ab802fb8e7014393
Michael Borck <michael@borck.dev>
2025-10-22 18:31:23 +0800

    Fix banner spacing

--- scripts/legacy/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Display the welcome banner every time the container starts
-cat /etc/banner.txt
-
-# Execute the command passed to the container
-# This allows 'docker compose run dfir /bin/bash' or
-# 'docker compose run dfir fls ...' to work as expected.
-exec "$@"
